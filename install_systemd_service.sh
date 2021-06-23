7a352923ca4326e3df101f8c58cdc1d01a3694bc
Lawrence Yu <lawy888@gmail.com>
2021-06-23 08:48:47 -0700

    fix systemd install

--- install_systemd_service.sh
+++ install_systemd_service.sh
@@ -6,8 +6,8 @@ sudo useradd -r -s /bin/false FlowController
 # make directories
 sudo mkdir -p /var/log/FlowController/joblogs
 sudo chown FlowController:FlowController /var/log/FlowController/joblogs
-sudo mkdir -p /var/lib/FlowController/ledgers
-sudo chown FlowController:FlowController /var/lib/FlowController/ledgers
+sudo mkdir -p /var/local/FlowController/ledgers
+sudo chown FlowController:FlowController /var/local/FlowController/ledgers
 sudo mkdir -p /var/lib/FlowController/sample_configs_and_jobs
 sudo chown FlowController:FlowController /var/lib/FlowController/sample_configs_and_jobs
 sudo mkdir -p /var/lib/FlowController/configs
